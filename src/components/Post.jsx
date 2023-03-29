a232464a843d34df82fabd9d101770adef972d59
geilinha <geilegiovana@outlook.com>
2023-03-29 01:03:15 -0400

    estiliza componente card

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,7 +1,40 @@
-import styles from './Post.module.css'
+import styles from "./Post.module.css";
 
 export function Post() {
-    return (
-        <h1>Hello world</h1>
-    )
-};
+  return (
+        <article className={styles.post}>
+        <header>
+            <div className={styles.author}>
+                <img
+                    className={styles.avatar}
+                    src="https://github.com/geilinha.png"
+                />
+                <div className={styles.authorInfo}>
+                    <strong>Geile Giovana</strong>
+                    <span>Web Developer</span>
+                </div>
+            </div>
+        
+
+            <time title="11 de Maio às 08:13h" dateTime="2022-05-11 08:13:30a">
+            Publicado há 1h
+            </time>
+        </header>
+
+            <div className={styles.content}>
+            <p>Fala pessoal 👋</p>
+            <p>
+                Finalmente finalizei meu novo site/portfólio. Foi um baita desafio
+                criar todo o design e codar na unha, mas consegui 💪🏻{" "}
+            </p>
+            <p>
+                Acesse e deixe seu feedback 👉<a href=""> devonlane.design</a>
+            </p>
+            <p>
+                <a href="">#uiux</a>{'   '} 
+                <a href="">#userexperience </a>
+            </p>
+            </div>
+        </article>
+  )
+}
