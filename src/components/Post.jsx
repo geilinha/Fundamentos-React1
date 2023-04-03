2e78e90ece78fd260eed2907221d88f7be27fdd4
geilinha <geilegiovana@outlook.com>
2023-04-03 02:18:54 -0400

    finalizacao layout

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,40 +1,55 @@
+import { Avatar } from "./Avatar";
+import { Comment } from "./Comment";
 import styles from "./Post.module.css";
 
 export function Post() {
   return (
-        <article className={styles.post}>
-        <header>
-            <div className={styles.author}>
-                <img
-                    className={styles.avatar}
-                    src="https://github.com/geilinha.png"
-                />
-                <div className={styles.authorInfo}>
-                    <strong>Geile Giovana</strong>
-                    <span>Web Developer</span>
-                </div>
-            </div>
-        
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <Avatar src="https://github.com/geilinha.png" />
+          <div className={styles.authorInfo}>
+            <strong>Geile Giovana</strong>
+            <span>Web Developer</span>
+          </div>
+        </div>
 
-            <time title="11 de Maio às 08:13h" dateTime="2022-05-11 08:13:30a">
-            Publicado há 1h
-            </time>
-        </header>
+        <time title="11 de Maio às 08:13h" dateTime="2022-05-11 08:13:30a">
+          Publicado há 1h
+        </time>
+      </header>
 
-            <div className={styles.content}>
-            <p>Fala pessoal 👋</p>
-            <p>
-                Finalmente finalizei meu novo site/portfólio. Foi um baita desafio
-                criar todo o design e codar na unha, mas consegui 💪🏻{" "}
-            </p>
-            <p>
-                Acesse e deixe seu feedback 👉<a href=""> devonlane.design</a>
-            </p>
-            <p>
-                <a href="">#uiux</a>{'   '} 
-                <a href="">#userexperience </a>
-            </p>
-            </div>
-        </article>
-  )
+      <div className={styles.content}>
+        <p>Fala pessoal 👋</p>
+        <p>
+          Finalmente finalizei meu novo site/portfólio. Foi um baita desafio
+          criar todo o design e codar na unha, mas consegui 💪🏻{" "}
+        </p>
+        <p>
+          Acesse e deixe seu feedback 👉<a href=""> devonlane.design</a>
+        </p>
+        <p>
+          <a href="">#uiux</a>
+          {"   "}
+          <a href="">#userexperience</a>
+        </p>
+      </div>
+
+      <form className={styles.commentForm}>
+        <strong>Deixe seu feedback</strong>
+
+        <textarea placeholder="Escreva um comentário..." />
+
+        <footer>
+          <button type="submit">Publicar</button>
+        </footer>
+      </form>
+
+      <div className={styles.commentList}>
+        <Comment />
+        <Comment />
+        <Comment />
+      </div>
+    </article>
+  );
 }
