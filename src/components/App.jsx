0a1b3c92554a7fa05073af51ddf604aa3412ba68
geilinha <geilegiovana@outlook.com>
2023-04-13 07:22:15 -0400

    useState

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,28 +1,78 @@
-import { Header } from './Header'
-import { Sidebar } from './Sidebar'
-import '../styles/global.css'
-import styles from './App.module.css'
-import { Post } from './Post'
+import { Header } from "./Header";
+import { Sidebar } from "./Sidebar";
+import "../styles/global.css";
+import styles from "./App.module.css";
+import { Post } from "./Post";
+
+//author = {url, name, office} = string
+//content = {conteudo} = string
+//publishAt = {data} = date
+
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/geilinha.png',
+      name: 'Geile Giovana',
+      role: 'Web Developer',
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala pessoal 👋' },
+      { type: 'paragraph', content: 'Finalmente finalizei meu novo site/portfólio. Foi um baita desafio criar todo o design e codar na unha, mas consegui 💪🏻', },
+      { type: 'paragraph', content: 'Acesse e deixe seu feedback' },
+      { type: 'link', content: 'devonlane.design' },
+    ],
+    publishedAt: new Date("2023-03-11 20:00:00"),
+  },
+
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/geilinha.png',
+      name: 'Geile Giovana',
+      role: 'Web Developer',
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala pessoal 👋' },
+      { type: 'paragraph', content: 'Finalmente finalizei meu novo site/portfólio. Foi um baita desafio criar todo o design e codar na unha, mas consegui 💪🏻', },
+      { type: 'paragraph', content: 'Acesse e deixe seu feedback' },
+      { type: 'link', content: 'devonlane.design' },
+    ],
+    publishedAt: new Date("2023-04-11 20:00:00"),
+  },
+];
 
 function App() {
   return (
     <div>
       <Header />
-    
+
       <div className={styles.whapper}>
         <Sidebar />
 
         <main>
-          <Post
-          />
-          <Post
-          />
+          {posts.map((post) => {
+            return (
+              (
+                <Post
+                  author={post.author}
+                  content={post.content}
+                  publishedAt={post.publishedAt}
+                />
+              ),
+              (
+                <Post
+                  author={post.author}
+                  content={post.content}
+                  publishedAt={post.publishedAt}
+                />
+              )
+            );
+          })}
         </main>
-
       </div>
     </div>
-
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,41 +1,61 @@
+import { format, formatDistanceToNow } from "date-fns";
+import ptBR from "date-fns/locale/pt-BR";
+import { useState } from "react";
 import { Avatar } from "./Avatar";
 import { Comment } from "./Comment";
 import styles from "./Post.module.css";
 
-export function Post() {
+export function Post({ author, publishedAt, content }) {
+  const [comments, setComment] = useState([
+    1,
+    2,
+  ])
+
+  function handleCreateNewComment() {
+    event.preventDefault()
+  
+    setComment([...comments, comments.length + 1]);
+  }
+
+const publishedAtFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
+  locale: ptBR,
+})
+
+const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
+  locale: ptBR,
+  addSuffix: true,
+})
   return (
     <article className={styles.post}>
       <header>
+
         <div className={styles.author}>
-          <Avatar src="https://github.com/geilinha.png" />
+          <Avatar src={author.avatarUrl} />
+        
           <div className={styles.authorInfo}>
-            <strong>Geile Giovana</strong>
-            <span>Web Developer</span>
+            <strong>{author.name}</strong>
+            <span>{author.role}</span>
           </div>
         </div>
 
-        <time title="11 de Maio às 08:13h" dateTime="2022-05-11 08:13:30a">
-          Publicado há 1h
+        <time title={publishedAtFormatted} dateTime={publishedAt.toISOString()}>
+          {publishedDateRelativeToNow}
         </time>
       </header>
 
       <div className={styles.content}>
-        <p>Fala pessoal 👋</p>
-        <p>
-          Finalmente finalizei meu novo site/portfólio. Foi um baita desafio
-          criar todo o design e codar na unha, mas consegui 💪🏻{" "}
-        </p>
-        <p>
-          Acesse e deixe seu feedback 👉<a href=""> devonlane.design</a>
-        </p>
-        <p>
-          <a href="">#uiux</a>
-          {"   "}
-          <a href="">#userexperience</a>
-        </p>
+        {content.map((line) => {
+          if(line.type == 'paragraph') {
+            return <p>{line.content}</p>;
+          } else if(line.type == 'link') {
+            return <p><a href="">{line.content}</a></p>;
+          }
+
+        })}
+
       </div>
 
-      <form className={styles.commentForm}>
+      <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
         <strong>Deixe seu feedback</strong>
 
         <textarea placeholder="Escreva um comentário..." />
@@ -46,9 +66,10 @@ export function Post() {
       </form>
 
       <div className={styles.commentList}>
-        <Comment />
-        <Comment />
-        <Comment />
+        {comments.map(comment => {
+          return <Comment />
+        })}
+        
       </div>
     </article>
   );
